e26c7c66fc8a7cf305f3524f25045c254af05a27
Raphael Betz <raphaelbetz@gmail.com>
2024-01-18 01:40:09 +0100

    rename images themselves without new folders, introduce debug variable for logging

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from pyzbar.pyzbar import decode
 from pyzbar.wrapper import ZBarSymbol
 
 
+debug_output = True
+
+
 def scan_barcodes(image_name) -> [str]:
     image_path = f"images/in/{image_name}"
     image = cv2.imread(image_path)
@@ -13,42 +16,44 @@ def scan_barcodes(image_name) -> [str]:
     barcode_results = []
     if barcodes:
         for barcode in barcodes:
-            print(barcode)
+            # if debug_output: print(barcode)
             # noinspection PyTestUnpassedFixture
             barcode_text = barcode.data.decode("utf-8")
             if validate_barcode(image_name, barcode_text):
                 barcode_results.append(barcode_text)
     else:
-        print(f"{child.name}: no barcode found.")
+        if debug_output: print(f"{child.name}: no barcode found.")
     return barcode_results
 
 
 def validate_barcode(filename, barcode_text) -> bool:
     if barcode_text.isdigit():
         if 1000000 < int(barcode_text) < 7000000:
-            print(f"{filename}: result: '{barcode_text}'.")
+            if debug_output: print(f"{filename}: result: '{barcode_text}'.")
             return True
         else:
-            print(f"{filename}: result: '{barcode_text}' not in range.")
+            if debug_output: print(f"{filename}: result: '{barcode_text}' not in range.")
     else:
-        print(f"{filename}: result: '{barcode_text}' no int.")
+        if debug_output: print(f"{filename}: result: '{barcode_text}' no int.")
     return False
 
 
 if __name__ == "__main__":
     path = './images/in/'
+    path_name = f"images/out/"
+    os.makedirs(path_name, exist_ok=True)
     previous_result = None
+    reset_count = 0
     for child in sorted(Path(path).iterdir()):
         if child.is_file():
             results = scan_barcodes(child.name)
             if len(results) == 1:
                 previous_result = results[0]
+                reset_count = 0
             elif len(results) >= 2:
                 previous_result = results[0]
+                reset_count = 0
                 for result in results[1:]:
                     previous_result += f"_{result}"
-            path_name = f"images/out/{str(previous_result)}/"
-            os.makedirs(path_name, exist_ok=True)
-            # new_image_path = os.path.join(os.path.dirname(image_path), new_image_name)
-            # os.rename(image_path, new_image_path)
-            shutil.copyfile(f"{path}{child.name}", f"{path_name}{child.name}")
+            reset_count += 1
+            shutil.copyfile(f"{path}{child.name}", f"{path_name}{previous_result}-{reset_count}.jpg")
