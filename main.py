2a448897fb8105f8ef489ec76e68a68a2e59c7e4
Raphael Betz <raphaelbetz@gmail.com>
2024-01-18 01:19:14 +0100

    refactor main.py for more detailed logging

--- main.py
+++ main.py
@@ -3,24 +3,36 @@ from pathlib import Path
 import shutil
 import cv2
 from pyzbar.pyzbar import decode
+from pyzbar.wrapper import ZBarSymbol
 
-def search_barcode(image_name):
+
+def scan_barcodes(image_name) -> [str]:
     image_path = f"images/in/{image_name}"
-    # print(image_path)
     image = cv2.imread(image_path)
-    barcodes = decode(image)
+    barcodes = decode(image, symbols=[ZBarSymbol.CODE128])
+    barcode_results = []
     if barcodes:
-        barcode_text = barcodes[0].data.decode("utf-8")
-        return barcode_text
-        path_name = f"images/out/{str(barcode_text)}/"
-        os.makedirs(path_name)
-        # new_image_path = os.path.join(os.path.dirname(image_path), new_image_name)
-        # os.rename(image_path, new_image_path)
-        shutil.copyfile(image_path, f"{path_name}/{image_name}")
-        print("Renamed image")
+        for barcode in barcodes:
+            print(barcode)
+            # noinspection PyTestUnpassedFixture
+            barcode_text = barcode.data.decode("utf-8")
+            if validate_barcode(image_name, barcode_text):
+                barcode_results.append(barcode_text)
     else:
-        return None
-        print("No barcode")
+        print(f"{child.name}: no barcode found.")
+    return barcode_results
+
+
+def validate_barcode(filename, barcode_text) -> bool:
+    if barcode_text.isdigit():
+        if 1000000 < int(barcode_text) < 7000000:
+            print(f"{filename}: result: '{barcode_text}'.")
+            return True
+        else:
+            print(f"{filename}: result: '{barcode_text}' not in range.")
+    else:
+        print(f"{filename}: result: '{barcode_text}' no int.")
+    return False
 
 
 if __name__ == "__main__":
@@ -28,12 +40,13 @@ if __name__ == "__main__":
     previous_result = None
     for child in sorted(Path(path).iterdir()):
         if child.is_file():
-            print(child.name)
-            result = search_barcode(child.name)
-            if result is not None:
-                if result.isdigit():
-                    if 1000000 < int(result) < 7000000:
-                        previous_result = int(result)
+            results = scan_barcodes(child.name)
+            if len(results) == 1:
+                previous_result = results[0]
+            elif len(results) >= 2:
+                previous_result = results[0]
+                for result in results[1:]:
+                    previous_result += f"_{result}"
             path_name = f"images/out/{str(previous_result)}/"
             os.makedirs(path_name, exist_ok=True)
             # new_image_path = os.path.join(os.path.dirname(image_path), new_image_name)
